f629b87697c8f2ff4a57d34a854952ad5b8b56c0
Kareem Mohamed <kareem3d.a@gmail.com>
2017-01-11 18:37:17 +0200

    Remove unused material design library

--- client/index.js
+++ client/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { browserHistory, applyRouterMiddleware, Router } from 'react-router';
 import useRelay from 'react-router-relay';
 
-import '../node_modules/react-mdl/extra/material';
 import Route from './routes/Route';
 
 const rootNode = document.createElement('div');
