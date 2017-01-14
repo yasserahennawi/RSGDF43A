a3fb975ea38bd93d835d696e1572a8df506ef348
Kareem Mohamed <kareem3d.a@gmail.com>
2017-01-14 12:34:31 +0200

    REFACTOR: Remove all client boilerplate code

--- client/index.js
+++ client/index.js
@@ -4,12 +4,10 @@ import ReactDOM from 'react-dom';
 import { browserHistory, applyRouterMiddleware, Router } from 'react-router';
 import useRelay from 'react-router-relay';
 
-import Route from './routes/Route';
-
 const rootNode = document.createElement('div');
 document.body.appendChild(rootNode);
 
 ReactDOM.render(
-  <Router history={browserHistory} routes={Route} render={applyRouterMiddleware(useRelay)} environment={Relay.Store} />,
+  <div></div>,
   rootNode
 );
